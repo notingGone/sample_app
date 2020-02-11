7a54fec547c3f5062fc536f7f4dc87e1b086972d
Elliott Woodward <elliottcwoodward@gmail.com>
2020-02-11 13:11:05 -0500

    Build out integration site layout test

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -4,20 +4,23 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   test 'layout links' do
     get root_path
     assert_template 'static_pages/home'
+    assert_select 'title', full_title
     assert_select 'a[href=?]', root_path, count: 2
     assert_select 'a[href=?]', help_path
     assert_select 'a[href=?]', about_path
     assert_select 'a[href=?]', contact_path
     assert_select 'a[href=?]', signup_path
     get contact_path
+    assert_template 'static_pages/contact'
     assert_select 'title', full_title('Contact')
-    get signup_path
-    assert_select 'title', full_title('Signup')
     get help_path
+    assert_template 'static_pages/help'
     assert_select 'title', full_title('Help')
     get about_path
+    assert_template 'static_pages/about'
     assert_select 'title', full_title('About')
-    get root_path
-    assert_select 'title', full_title
+    get signup_path
+    assert_template 'users/new'
+    assert_select 'title', full_title('Signup')
   end
 end
